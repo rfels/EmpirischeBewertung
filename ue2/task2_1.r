0b59263b416f07407ffaf0a643c87fc73c4f5df3
rfels <robert.fels@fu-berlin.de>
2014-05-05 00:41:08 +0200

    added ue2 task 2-1 a)

---
+++ ue2/task2_1.r
@@ -0,0 +1,38 @@
+####### task 2_1 a) + b) #######
+#read data into data.frame
+origFile = read.delim("data/junit200.tsv",as.is=T)
+is.data.frame(origFile)
+
+#add developer and file as factor
+origFile$developerf <- as.factor(origFile$developer)
+origFile$filef <- as.factor(origFile$file)
+colnames(origFile)
+origFile$developerf;origFile$filef
+
+#delete description column
+origFile = subset(origFile,select=-description)
+colnames(origFile)
+
+#get an overview over data
+names(origFile)
+nrow(origFile)
+str(origFile)
+summary(origFile)
+
+# function to get that type of data set
+myread.cvsdata = function(csvFile) {
+	result= read.delim(csvFile,as.is=T)
+	result$developerf <-as.factor(result$developer)
+	result$filef <-as.factor(result$file)
+	result = subset(result, select=-description)
+	return(result)
+}
+
+summary(myread.cvsdata("data/junit.tsv"))
+summary(myread.cvsdata("data/jikes.tsv"))
+summary(myread.cvsdata("data/zile.tsv"))
+	
+####### task 2_1 c) #######
+
+
+
