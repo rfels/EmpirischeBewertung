e341427b970614139db890eefc43a91c69e0f600
Christoph Krüger <christoph.krueger@fu-berlin.de>
2014-05-05 01:24:34 +0200

    add task 2.1 d)

--- ue2/task2_1.r
+++ ue2/task2_1.r
@@ -37,3 +37,9 @@ summary(myread.cvsdata("data/zile.tsv"))
 nrow(unique(origFile["developer"]))
 sort(table(origFile["developer"]))
 table(unique(origFile[c("developer","file")])["developer"])
+
+####### task 2_1 d) #######
+myanalyze.cvsdata<-function(x){
+list("#developers"=nrow(unique(x["developer"])),"#commits"=sort(table(x["developer"])),
+"#of files with commits"=table(unique(x[c("developer","file")])["developer"]))
+}
