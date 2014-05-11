68ef96a3a9a3ef9fbbe75e6d46881c2457f2eee9
Christoph Krüger <christoph.krueger@fu-berlin.de>
2014-05-11 23:47:46 +0200

    add task3-1 c-e and beginning of f

--- ue3/task3_1.r
+++ ue3/task3_1.r
@@ -28,4 +28,24 @@ junit$tstamp3 = as.numeric(junit$tstamp2)
 diff(junit$tstamp2[1:2])
 # diff in seconds
 diff(junit$tstamp3[1:2])
+#### task3-1 c) ####
+#days and hours extracting
+junit$wday <-weekdays(junit$tstamp2)
+junit$hour <- strftime(as.POSIXlt(junit$tstamp2,t ="UTC"),format="%H")
+
+#### task3-1 d) ####
+#plots
+plot(table(junit$wday))
+plot(table(junit$hour))
+
+#### task3-1 e) ####
+#labels
+plot(factor(junit$wday,labels=strtrim(levels(factor(junit$wday)),2)))
+#### task3-1 f) ####
+#boxplot
+
+boxplot(log(lines_add+1,2)~developer,junit)
+boxplot(log(lines_del+1,2)~developer,junit)
+
+
 
